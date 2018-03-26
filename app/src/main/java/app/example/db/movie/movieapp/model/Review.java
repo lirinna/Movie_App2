9d92d23aedfd5ee42d05d1215b27f2b586fcab55
lirinna <37187549+lirinna@users.noreply.github.com>
2018-03-26 22:32:31 +0200

    Add Review and Trailer model class

---
+++ app/src/main/java/app/example/db/movie/movieapp/model/Review.java
@@ -0,0 +1,8 @@
+package app.example.db.movie.movieapp.model;
+
+/**
+ * Created by Katy on 26.03.2018.
+ */
+
+public class Review {
+}
---
+++ app/src/main/java/app/example/db/movie/movieapp/model/Trailer.java
@@ -0,0 +1,32 @@
+package app.example.db.movie.movieapp.model;
+
+/**
+ * Created by Katy on 26.03.2018.
+ */
+
+
+public class Trailer {
+
+    private String mKey;
+    private String mName;
+
+    public Trailer() {
+
+    }
+
+    public String getmKey() {
+        return mKey;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmKey(String mKey) {
+        this.mKey = mKey;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+}
